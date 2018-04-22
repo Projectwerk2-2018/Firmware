ad090a40a5ab855d098ba30aa6d621423364af65
Elias Verstappe <elias.verstappe@gmail.com>
2018-04-22 19:06:07 +0200

    I commited without saving all changes, now all saved so commit sensor_data.cpp

--- src/lib/sensor_data.cpp
+++ src/lib/sensor_data.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // default constructor returns a json-object
 
 // SensorData(double temperature, int motion, double humidity){
-SensorData::SensorData(){
+string SensorData::SensorData(){
     return  "{\"temperature\": {\"value\":" + std::to_string(temperature) + ", \"unit\": \"°C\"}, " +
             "\"motion\": " + std::to_string(motion) + ", "
             "\"humidity\": {\"value\":" + std::to_string(humidity) + ", \"unit\": \"%\"}}" ;
