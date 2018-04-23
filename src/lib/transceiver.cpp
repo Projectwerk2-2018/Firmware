21d8bd6838872dbfef8fafb82747dab280a9a2b4
Elias Verstappe <elias.verstappe@gmail.com>
2018-04-23 17:29:39 +0200

    remove unneeded comments

--- src/lib/transceiver.cpp
+++ src/lib/transceiver.cpp
@@ -1,39 +1,6 @@
 #include "transceiver.h"
 #include <string>
 
-/*
-
-double Transceiver::get_temparure()
-{
-    return temperature;
-}
-
-bool Transceiver::get_motion()
-{
-    return motion;
-}
-
-double Transceiver::get_humidity()
-{
-    return humidity;
-}
-
-void send(double temperature, bool motion, double humidity)
-{
-    std::string Transceiver::to_string()
-    {
-    return "The Temparture is " + std::to_String(temperature) + " "
-            ", movement is " + std::to_string(motion) + " "
-            ", humidity is " + std::to_string(humidity) + "."
-    }   
-}
-
-void Transceiver::set_send(void send)
-{
-    this->send = send;
-}
-
-*/
 
 void Transceiver::send(){
     
--- src/lib/transceiver.h
+++ src/lib/transceiver.h
@@ -2,23 +2,7 @@
 
 class Transceiver
 {
-    /*
-    private:
-        double temperature;
-        bool motion;
-        double humidity;
     
-    public:
-        double get_temparure();
-        bool get_motion();
-        double get_humidity();
-
-    public:
-        void set_send(void send);
-
-void send(double temperature, bool motion, double humidity);
-    */
-   
     public:
 
     virtual void send();
