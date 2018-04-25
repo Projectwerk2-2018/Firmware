045bd6405eedcf1194a5703bee8803f424cbc61a
Elias Verstappe <elias.verstappe@gmail.com>
2018-04-25 21:19:35 +0200

    Add print to terminal functionality

--- src/lib/terminal.cpp
+++ src/lib/terminal.cpp
@@ -1,6 +1,14 @@
 #include "terminal.h"
+#include "sensor_data.h"
 
 void Terminal::get_send()
 {
   
+}
+
+void displayData(SensorData dataCluster){
+
+    string Str = dataCluster.toString() + "\r\n";
+
+    printf(Str.c_str());
 }
