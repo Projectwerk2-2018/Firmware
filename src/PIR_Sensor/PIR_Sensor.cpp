23f74f73628b15cfc89fd7fe8ba43894b117195b
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-04-26 09:03:08 +0200

    deleted motion class and finished pir class

--- src/PIR_Sensor/PIR_Sensor.cpp
+++ src/PIR_Sensor/PIR_Sensor.cpp
@@ -1,16 +1,16 @@
-/*#include "pir_sensor.h"
+#include "PIR_Sensor.h"
+#include "mbed.h"
 
-    PIRSensor::motion(PinName pin) : _interrupt(pin) {
-        _interrupt.rise(this, &PIRSensor::status);
+    PIRSensor::PIRSensor(PinName pin) : sensorInterrupt(pin) {
+        sensorInterrupt.rise(callback(this, &PIRSensor::isr_movement_detected));
     }
 
-    void PIRSensor::status() {
+    void PIRSensor::isr_movement_detected() {
         numberOfMovements = numberOfMovements + 1;
     }
 
-    int PIRSensor::read() {
+    int PIRSensor::get_number_of_movements() {
         int tempNumberOfMovements = numberOfMovements;
         numberOfMovements = 0;
         return tempNumberOfMovements;
     }
-*/
