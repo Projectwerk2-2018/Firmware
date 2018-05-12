f653a319a3f32f40ec93af6bc98050ab97d90e8f
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-05-12 17:29:00 +0200

    change pir class so it works with timestamps.

--- src/PIR_Sensor/PIR_Sensor.cpp
+++ src/PIR_Sensor/PIR_Sensor.cpp
@@ -2,16 +2,36 @@
 #include "mbed.h"
 
     PIRSensor::PIRSensor(PinName pin) : sensorInterrupt(pin) {
-        sensorInterrupt.rise(callback(this, &PIRSensor::isr_movement_detected));
-        this->numberOfMovements = 0;
+        sensorInterrupt.rise(callback(this, &PIRSensor::positive_edge_detected));
+        sensorInterrupt.fall(callback(this, &PIRSensor::negative_edge_detected));
+        set_time(1256729737);  
+        this ->positiveEdge = 0;
+        this ->negativeEdge = 0;
+        this ->timeOn = 0;
     }
 
-    void PIRSensor::isr_movement_detected() {
-        numberOfMovements = numberOfMovements + 1;
+    void PIRSensor::positive_edge_detected() {
+        time_t seconds = time(NULL);
+        positiveEdge = seconds;
+    }
+
+    void PIRSensor::negative_edge_detected() {
+        time_t seconds = time(NULL);
+        negativeEdge = seconds;
+    }
+
+    void PIRSensor::calculate_time_on(){
+        timeOn = negativeEdge - positiveEdge;
+        negativeEdge = 0;
+        positiveEdge = 0;
+    }
+
+    int PIRSensor::get_time_on(){
+        int tempTimeOn = timeOn;
+        timeOn = 0;
+        return tempTimeOn;
     }
 
     int PIRSensor::get_number_of_movements() {
-        int tempNumberOfMovements = numberOfMovements;
-        numberOfMovements = 0;
-        return tempNumberOfMovements;
+
     }
--- test-files/test-pir-sensor.cpp
+++ test-files/test-pir-sensor.cpp
@@ -8,13 +8,12 @@ Serial device(USBTX, USBRX); // tx, rx
 
 int main(){
 
-    PIRSensor motionDetector(D0);
+    PIRSensor motionDetector(A0);
     device.baud(19200);
 
     while(1){   
-    int numberOfMovements = motionDetector.get_number_of_movements();
-        device.printf("Number of movements : %i\r\n",numberOfMovements);
-        wait_ms(5000);
+        device.printf("time on: %i\r\n",motionDetector.get_time_on());
+        wait_ms(1000);
     }
 
 }
