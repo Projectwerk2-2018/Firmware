60a0301e79e32f895c6cb437220e76a5ec78fafe
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-05-20 16:23:19 +0200

    add temp class and refactor

--- src/Temperature/temperature_sensor.cpp
+++ src/Temperature/temperature_sensor.cpp
@@ -1,12 +1,36 @@
 #include "temperature_sensor.h"
 
+I2C sensorI2C(PC_1, PC_0); //PC_1=SDA, PC_0=SCL
+silabs::SILABS_RHT rhtSensor(&sensorI2C);
+Serial device(USBTX, USBRX);
 
-Temperature::Temperature() : i2c(D6, D7), rhtSensor(&i2c)
-{
+volatile bool busChecked = false;
 
+void respondedCallback( void ) {
+    busChecked = true;
 }
 
-double Temperature::get_temperature_value()
-{
-    return rhtSensor.get_temperature()/1000;
+int main() {
+
+    printf("Starting rhtSensor measurement\r\n");
+    int retval = rhtSensor.check_availability(si7013, respondedCallback);
+    printf("Available? %s\r\n", (!retval ? "SUCCESS" : "FAIL"));
+ 
+    //    while(busChecked == false) sleep();
+ 
+    //    busChecked = false;
+    while (true) {
+        retval = rhtSensor.measure(si7013, respondedCallback);
+        printf("Measure? %s\r\n", (!retval ? "SUCCESS" : "FAIL"));
+ 
+    //while(busChecked == false)  sleep();
+
+    if(rhtSensor.get_active()) {
+        printf("Temperature: %d.%03d degC\r\n", rhtSensor.get_temperature()/1000, rhtSensor.get_temperature()%1000);
+        printf("Humidity: %d.%03d %\r\n", rhtSensor.get_humidity()/1000, rhtSensor.get_humidity()%1000);
+    }else{
+        printf("No sensor found\r\n");
+    }
+        wait_ms(5000);
+    } 
 }
