6d93d3fa93e49f0a1c218afc82708bc704079692
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-05-22 20:34:02 +0200

    add temp humidity cpp class file

--- src/TemperatureHumidity/temperature_humidity_sensor.cpp
+++ src/TemperatureHumidity/temperature_humidity_sensor.cpp
@@ -7,7 +7,7 @@
     void TemperatureHumidity::read_values() {
         if(!rhtSensor.check_availability(si7013, nullptr) &&
             !rhtSensor.measure(si7013, nullptr)){
-            temperature = ((rhtSensor.get_temperature()/1000)+(rhtSensor.get_temperature()%1000));
+            temperature = ((rhtSensor.get_temperature()/1000.0)+(rhtSensor.get_temperature()%1000));
             humidity = ((rhtSensor.get_humidity()/1000)+(rhtSensor.get_humidity()%1000));
         }
     }
