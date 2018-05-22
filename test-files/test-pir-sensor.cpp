c1029824bd8231ea1c1fc672af56e69134f5e755
Daan Ver Eecke <daan.vereecke.dv@gmail.com>
2018-05-22 20:34:25 +0200

    add separate pir and test/humidity test files

--- test-files/test-pir-sensor.cpp
+++ test-files/test-pir-sensor.cpp
@@ -1,5 +1,4 @@
 #include "pir_sensor/pir_sensor.h"
-#include "TemperatureHumidity/temperature_humidity_sensor.h"
 #include "mbed.h"
 #include <iostream>
 
@@ -9,10 +8,7 @@ Serial device(USBTX, USBRX); // tx, rx
 
 int main(){
 
-    I2C sensorI2C(PC_1, PC_0);
     PIRSensor motionDetector(A0);
-    TemperatureHumidity tempsensor(&sensorI2C);
-    device.baud(19200);
     //start
     motionDetector.start();
         
@@ -22,8 +18,6 @@ int main(){
         motionDetector.stop();
         //calculate
         device.printf("percentage: %i\r\n",motionDetector.get_percentage_movement());
-        device.printf("temperature: %i\r\n",tempsensor.get_temperature());
-        device.printf("humidity: %i\r\n",tempsensor.get_humidity());
         // start
         motionDetector.start();
         wait_ms(1000);
